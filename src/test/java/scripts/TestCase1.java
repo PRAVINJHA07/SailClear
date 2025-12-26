832732b8a88de286b55574df25874d5026b9e091
Pravin <pravinkrjha07@gmail.com>
2025-12-26 14:26:24 +0530

    Code Updated

--- src/test/java/scripts/TestCase1.java
+++ src/test/java/scripts/TestCase1.java
@@ -1,10 +1,12 @@
 package scripts;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -17,7 +19,16 @@ public class TestCase1 {
 		driver.manage().window().maximize();
 		driver.get("https://sailclear.com/");
 		
-		driver.findElement(By.xpath("//button[text()='X']")).click();
+		List<WebElement> closeBtn =
+		        driver.findElements(By.xpath("//button[text()='X']"));
+
+		if (!closeBtn.isEmpty()) {
+		    closeBtn.get(0).click();
+		    System.out.println("Popup closed");
+		} else {
+		    System.out.println("Popup not displayed");
+		}
+
 		Thread.sleep(3000);
 		
 		
